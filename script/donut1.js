5d123225537645b05448a2916a1c31d7cd11a00c
Hippolyte <Hippolyte@bois2scie.com>
2022-12-11 23:25:30 +0100

    template quasi fini

--- script/donut1.js
+++ script/donut1.js
@@ -1,4 +1,4 @@
-var data = {
+const data1 = {
   labels: ["Etat", "Ingenieurs","Constructeurs"],
   datasets: [{
     datalabels: {
@@ -26,8 +26,8 @@ var data = {
   }]
 }
 
-var data2 = {
-  labels: ["Etat", "Ingenieurs","Constructeurs"],
+const data2 = {
+  labels: ["a", "b","c"],
   datasets: [{
     datalabels: {
       listeners: {
@@ -46,13 +46,43 @@ var data2 = {
     backgroundColor: ["#02166a", "#ffb300", "#01809e"],
     hoverBorderColor: "#fff",
     borderColor: ["#02166a", "#ffb300", "#01809e"],
-    data: [50,25,25],
+    data: [15,70,15],
     hoverBorderWidth: 3,
     borderWidth: [0,0,0],
 
     offset: [0,0,0]
   }]
 }
+
+const data3 = {
+  labels: ["d", "e","f"],
+  datasets: [{
+    datalabels: {
+      listeners: {
+        click: function(context, event) {
+          // Receives `click` events only for labels of the first dataset.
+          // The clicked label index is available in `context.dataIndex`.
+          console.log('label ' + context.dataIndex + ' has been clicked!');
+          console.log('mouse is at position x:', event.x, 'and y:', event.y);
+
+          if (event.native.ctrlKey) {
+            console.log('control key is pressed!');
+          }
+        }
+      }
+    },
+    backgroundColor: ["#02166a", "#ffb300", "#01809e"],
+    hoverBorderColor: "#fff",
+    borderColor: ["#02166a", "#ffb300", "#01809e"],
+    data: [33,33,33],
+    hoverBorderWidth: 3,
+    borderWidth: [0,0,0],
+
+    offset: [0,0,0]
+  }]
+}
+
+var data= data1;
 
 var options = {
   cutoutPercentage: 60,
@@ -82,7 +112,7 @@ var options = {
 }
 
 
-const config = {
+var config = {
   type: "doughnut",
   data,
   options
@@ -122,6 +152,7 @@ $("h4").click(function(){
   $("h4").css('background-color', 'darkslateblue');
   $(this).addClass("active");
   $(this).css('background-color', 'indianred');
+  changeDonut($(this)[0].innerHTML);
 
   donut.destroy();
   donut = new Chart(
@@ -129,3 +160,18 @@ $("h4").click(function(){
     config
   );
 });
+
+function changeDonut(name){
+  if (name==="Sécurité") {
+    data=data1
+  }else if(name==="Économie"){
+    data=data2
+  }else{
+    data=data3
+  }
+  config = {
+    type: "doughnut",
+    data,
+    options
+  };
+}
